fca6c90b4ed8fc6dd02ba5e7c8a971e24bfa32fd
Abhishek Choudhary <acabhishek942@gmail.com>
2018-06-30 23:55:06 +0530

    writing duplicate image urls to a json file

--- src/data_preprocessing/data_parser.py
+++ src/data_preprocessing/data_parser.py
@@ -23,15 +23,18 @@ def onlyOriginalImageURL(imageURLString):
 #     print(training_data.index)
 #     break;
 
-def csv_reader(path, chunksize=10**4):
+def csv_reader(path, chunksize=10**4, columns_to_read=None, mark_duplicates=True):
     """
     Returns the mentioned number of rows from csv file.
 
     param path: Path of the csv file
     param chunksize: number of records to process at a time.
             Default value : 10**4
+    param columns_to_read: specify the columns to read.
+            Default value: None(signifies all columns to be read)
+    param mark_duplicates: mark duplicates in the dataframe
     """
-    for chunk in pd.read_csv(path, chunksize=chunksize):
+    for chunk in pd.read_csv(path, usecols=columns_to_read, mangle_dupe_cols=mark_duplicates, chunksize=chunksize):
         yield chunk
 
 def shorten_imageURLString(chunk):
@@ -50,3 +53,12 @@ def csv_writer_append(path, chunk):
         chunk.to_csv(path, index=False)
     else:
         chunk.to_csv(path, mode='a', index=False, header=False)
+
+duplicate_image_url_dict = {} #needs-attention Need to see how to store this variable
+def make_duplicate_image_url_dict(chunk, column_name, chunksize=10**4):
+    for index, row in chunk.iterrows():
+        if isinstance(row['imageUrlStr'], str):
+            if row['imageUrlStr'] in duplicate_image_url_dict:
+                duplicate_image_url_dict[row['imageUrlStr']].append(index)
+            else:
+                duplicate_image_url_dict[row['imageUrlStr']] = [index]
--- src/driver.py
+++ src/driver.py
@@ -1,10 +1,29 @@
-from csv_parser import data_parser
+from data_preprocessing import data_parser
+
+import json
 
 original_path = '/Users/ac/Documents/2oq-c1r.csv'
 reduced_to_original_image_path = '../reduced_data/reduced_to_original_image_url.csv'
+duplicate_image_url_json_path = '../reduced_data/duplicate_image_url.json'
 
 # Code snippet to reduce the size of dataset by shortening imageURLString
-# Reduced from around 6GB to 4.4GB 
-for smallchunk in data_parser.csv_reader('/Users/ac/Documents/2oq-c1r.csv', chunksize=10**4):
-    smallchunk['imageUrlStr'] = data_parser.shorten_imageURLString(smallchunk)
-    data_parser.csv_writer_append(reduced_to_original_image_path, smallchunk)
+# Reduced from around 6GB to 4.4GB
+# for smallchunk in data_parser.csv_reader(original_path, chunksize=10**4):
+#     smallchunk['imageUrlStr'] = data_parser.shorten_imageURLString(smallchunk)
+#     data_parser.csv_writer_append(reduced_to_original_image_path, smallchunk)
+
+# marking products having same imageURLString as copies of one another
+"""
+Generate a dictionary like {imageURLString: [list of all repeating indexes]}
+"""
+# duplicate_image_url_dict = {}
+# for smallchunk in data_parser.csv_reader(reduced_to_original_image_path, columns_to_read=['imageUrlStr'], chunksize=10**4):
+#     for index, row in smallchunk.iterrows():
+#         if isinstance(row['imageUrlStr'], str):
+#             if row['imageUrlStr'] in duplicate_image_url_dict:
+#                 duplicate_image_url_dict[row['imageUrlStr']].append(index)
+#             else:
+#                 duplicate_image_url_dict[row['imageUrlStr']] = [index]
+#
+# with open(duplicate_image_url_json_path, 'w') as fp:
+#     json.dump(duplicate_image_url_dict, fp)
