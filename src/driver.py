345c2b2e2597277155e3ebb209ef51b3a31ed2fc
Abhishek Choudhary <acabhishek942@gmail.com>
2018-07-01 14:53:41 +0530

    checking that there arent any duplicates in the data after dropping the duplicates

--- src/driver.py
+++ src/driver.py
@@ -20,14 +20,16 @@ Reduced from around 6GB to 4.4GB
 """
 Generate a dictionary like {imageURLString: [list of all repeating indexes]}
 """
-# duplicate_image_url_dict = {}
-# for smallchunk in data_parser.csv_reader(reduced_to_original_image_path, columns_to_read=['imageUrlStr'], chunksize=10**4):
-#     for index, row in smallchunk.iterrows():
-#         if isinstance(row['imageUrlStr'], str):
-#             if row['imageUrlStr'] in duplicate_image_url_dict:
-#                 duplicate_image_url_dict[row['imageUrlStr']].append(index)
-#             else:
-#                 duplicate_image_url_dict[row['imageUrlStr']] = [index]
+duplicate_image_url_dict = {}
+for smallchunk in data_parser.csv_reader(reduced_drop_duplicate_rows_path, columns_to_read=['imageUrlStr'], chunksize=10**4):
+    for index, row in smallchunk.iterrows():
+        if isinstance(row['imageUrlStr'], str):
+            if row['imageUrlStr'] in duplicate_image_url_dict:
+                duplicate_image_url_dict[row['imageUrlStr']].append(index)
+            else:
+                duplicate_image_url_dict[row['imageUrlStr']] = [index]
+for key in duplicate_image_url_dict:
+    print duplicate_image_url_dict[key]
 #
 # with open(duplicate_image_url_json_path, 'w') as fp:
 #     json.dump(duplicate_image_url_dict, fp)
@@ -53,19 +55,19 @@ Remove all entries from duplicate_image_url_json_path that does not repeat
 Drop rows from dataset that have same image url and write to new csv file.
 Took about 10 mins to execute and reduced dataset size to 1.3GB
 """
-with open(reduced_image_url_json_path) as fp:
-    reduced_image_url_dict = json.load(fp)
-
-loop_counter = 0
-for smallchunk in data_parser.csv_reader(reduced_to_original_image_path, chunksize=10**4):
-    smallchunk_backup = smallchunk
-    indexes_to_drop = []
-    for index, row in smallchunk.iterrows():
-        if row['imageUrlStr'] in reduced_image_url_dict:
-            indexes_to_drop_temp = reduced_image_url_dict[row['imageUrlStr']][1:]
-            for potential_drop_index in indexes_to_drop_temp:
-                if potential_drop_index in range(loop_counter*(10**4), 9999 + loop_counter*(10**4)):
-                    indexes_to_drop.append(potential_drop_index - (loop_counter*(10**4)))
-    smallchunk_backup.drop(smallchunk_backup.index[list(set(indexes_to_drop))], inplace=True)
-    data_parser.csv_writer_append(reduced_drop_duplicate_rows_path, smallchunk_backup)
-    loop_counter += 1
+# with open(reduced_image_url_json_path) as fp:
+#     reduced_image_url_dict = json.load(fp)
+#
+# loop_counter = 0
+# for smallchunk in data_parser.csv_reader(reduced_to_original_image_path, chunksize=10**4):
+#     smallchunk_backup = smallchunk
+#     indexes_to_drop = []
+#     for index, row in smallchunk.iterrows():
+#         if row['imageUrlStr'] in reduced_image_url_dict:
+#             indexes_to_drop_temp = reduced_image_url_dict[row['imageUrlStr']][1:]
+#             for potential_drop_index in indexes_to_drop_temp:
+#                 if potential_drop_index in range(loop_counter*(10**4), 9999 + loop_counter*(10**4)):
+#                     indexes_to_drop.append(potential_drop_index - (loop_counter*(10**4)))
+#     smallchunk_backup.drop(smallchunk_backup.index[list(set(indexes_to_drop))], inplace=True)
+#     data_parser.csv_writer_append(reduced_drop_duplicate_rows_path, smallchunk_backup)
+#     loop_counter += 1
